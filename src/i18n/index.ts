cbb1dc061ea9c516dd821fa4dbcca0b989f1e848
Joe Petrakovich <jpetrakovich@gmail.com>
2026-01-08 08:35:05 +0900

    added chinese translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,17 +1,19 @@
 import i18next from 'i18next';
 import en from './en.json';
 import ja from './ja.json';
+import zh from './zh.json';
 import { getLanguage } from 'obsidian';
 
 export const i18n = i18next.createInstance();
 const lng = getLanguage();
 
-i18n.init({
+void i18n.init({
   lng,
   fallbackLng: 'en',
   resources: {
     en: { translation: en },
-	ja: { translation: ja }
+	ja: { translation: ja },
+	zh: { translation: zh }
   },
   interpolation: {
     escapeValue: false
